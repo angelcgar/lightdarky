540f9d47f04c02d98c17eb9ac69a9c2e5bd25292
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2025-08-19 23:08:53 -0600

    feat: añadir un ejemplo funcional de el paquete

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import './style.css'
+import { initThemeToggle } from './themeToggle.ts'
+
+document.querySelector('#app')!.innerHTML = `
+    <h1 class="text-red-900 dark:text-blue-900">Hello Vite + TypeScript!</h1>
+    <div class="card">
+        <button id="counter">Click me</button>
+    </div>
+    <p class="read-the-docs">
+        Click on the Vite and TypeScript logos to learn more
+    </p>
+`
+
+initThemeToggle({
+    buttonSelector: '#counter',
+    defaultScheme: 'light'
+});
