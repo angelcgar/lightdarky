bbe2c982e9a8e6e374beffd50f1900043fafb759
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2025-08-21 22:32:02 -0600

    refactor: eliminar lineas que no estaba utilizando

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
-import './style.css'
-import { initThemeToggle } from './themeToggle.ts'
+// import './style.css'
+// import { initThemeToggle } from './themeToggle'
 
-document.querySelector('#app')!.innerHTML = `
-    <h1 class="text-red-900 dark:text-blue-900">Hello Vite + TypeScript!</h1>
-    <div class="card">
-        <button id="counter">Click me</button>
-    </div>
-    <p class="read-the-docs">
-        Click on the Vite and TypeScript logos to learn more
-    </p>
-`
+// document.querySelector('#app')!.innerHTML = `
+//     <h1 class="text-red-900 dark:text-blue-900">Hello Vite + TypeScript!</h1>
+//     <div class="card">
+//         <button id="counter">Click me</button>
+//     </div>
+//     <p class="read-the-docs">
+//         Click on the Vite and TypeScript logos to learn more
+//     </p>
+// `
 
-initThemeToggle({
-    buttonSelector: '#counter',
-    defaultScheme: 'light'
-});
+// initThemeToggle({
+//     buttonSelector: '#counter',
+//     defaultScheme: 'light'
+// });
